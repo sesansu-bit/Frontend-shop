10c573577167e2969d0938d730c3680b8447d2da
sesansu-bit <sesansuswain5@gmail.com>
2025-05-16 01:53:57 +0530

    merged remote and local

--- src/useFetchItems.js
+++ src/useFetchItems.js
@@ -27,7 +27,7 @@ const useAppFetch = () => {
 
     dispatch(fetchStatusActions.markFetchingStarted());
 
-    fetch("https://redux-backend-haqh.onrender.com/", { signal })
+fetch("https://redux-backend-haqh.onrender.com/api/items", { signal })
       .then((res) => res.json())
       .then(({ items }) => {
         dispatch(fetchStatusActions.markFetchingFinished());
